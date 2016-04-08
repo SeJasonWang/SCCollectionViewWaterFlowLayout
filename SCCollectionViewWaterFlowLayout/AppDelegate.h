20e1093532acc24f5d175b8052f18b233db9fff8
sichenwang <52870104@qq.com>
2016-04-08 12:21:23 +0800

    Initial Commit

---
+++ SCCollectionViewWaterFlowLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCCollectionViewWaterFlowLayout
+//
+//  Created by sichenwang on 16/4/8.
+//  Copyright © 2016年 sichenwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
